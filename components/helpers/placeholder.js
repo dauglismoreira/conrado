b91025693d5923baec1851bf1fa7f6df428ae38e
Buenexx <fernando@impacte.com.br>
2024-02-20 08:34:26 -0300

    feat: placeholder

---
+++ components/helpers/placeholder.js
@@ -0,0 +1,7 @@
+import positive from "../../assets/images/placeholder-primary.jpg"
+import negative from "../../assets/images/placeholder-secundary.jpg"
+
+export const placeholder = {
+    positive: positive.src,
+    negative: negative.src
+}
